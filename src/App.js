464c2b33eb1895096a7432aac2f8b41f5e77812d
ajaysmartsafex <ajaysmartsafex@gmail.com>
2025-06-30 09:10:49 +0530

    react code new repositorie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TodoList from './components/containers/todolist/TodoList';
+import UserList from './components/containers/userlists/UserList';
+import Heders from './components/navbar/Heders';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heders />
+      <TodoList />
+      <UserList />
     </div>
   );
 }
